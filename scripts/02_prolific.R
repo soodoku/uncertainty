f94eaf42b26b09ad90c4cab1f6158400971a0eb8
***** <gsood07@gmail.com>
2023-04-30 21:14:15 -0700

    draft analysis

--- scripts/02_prolific.R
+++ scripts/02_prolific.R
@@ -6,13 +6,26 @@ library(ggplot2)
 library(knitr)
 
 # Load dat
-uncert <- read_csv("data/prolific/Bot research_April 30, 2023_18.01.csv")
- 
+uncert <- read_csv("data/prolific/Bot research_April 30, 2023_21.46.csv")
+
+# Prolific
+prolific_1 <- read_csv("data/prolific/prolific_export_644c0bbca40d3cd7cb3c7c7f.csv")
+prolific_2 <- read_csv("data/prolific/prolific_export_644d3a9ebcd1c388bd1a262e.csv")
+prolific_3 <- read_csv("data/prolific/prolific_export_6440131f3fd384272e37636e.csv")
+
+prolific   <- rbind(prolific_1, prolific_2, prolific_3)
+
 # Filter attn_check
 uncert$attn_checkc <- uncert$attn == "Extremely interested,Very interested"
 
+# Left join
+all_dat <- uncert %>% 
+  left_join(prolific, by = c("prolific_id" = "Participant id"))
+
 # Filter those who consent and not preview
-fin_dat <- uncert[uncert$consent == 'Yes' & uncert$DistributionChannel != "preview" & !is.na(uncert$prolific_id) & uncert$attn_checkc, ]
+fin_dat <- all_dat %>% 
+  filter(consent == 'Yes' & DistributionChannel != "preview" & !is.na(prolific_id) & attn_checkc)
+
 
 ## Recoding
 ## -----------
@@ -22,7 +35,7 @@ fin_dat$num1c <- fin_dat$num1 == "45 miles"
 fin_dat$num2c <- fin_dat$num2 == "500"
 fin_dat$num3c <- fin_dat$num3 == "44"
 fin_dat$num4c <- fin_dat$num4 == "$150"
-fin_dat$num5c <- fin_dat$num5 == "$9,000"
+fin_dat$num5c <- fin_dat$num5 == "$9000"
 fin_dat$num6c <- fin_dat$num6 == "10"
 
 fin_dat$numeracy <- with(fin_dat, rowMeans(cbind(num1c, num2c, num3c, num4c, num5c, num6c)))
@@ -33,7 +46,7 @@ num_mean_se = fin_dat %>%
   group_by(key) %>%
   summarize(avg = mean(value), se = sqrt(avg*(1 - avg)/n()), low_ci = avg - 2*se, hi_ci = avg + 2*se)
 
-num_mean_se$lab <- c("Overdraft", "Dice", "Disease", "Sofa Sale", "Car", "Lottery")
+num_mean_se$lab <- c("Train", "Dice", "Father-Son", "Sofa Sale", "Car", "Lottery")
 
 kable(num_mean_se[, c("lab", "avg")])
 
@@ -54,57 +67,42 @@ ggplot(num_mean_se,
         axis.ticks.y = element_blank(),
         axis.title.x = element_text(vjust = -1, margin = margin(10, 0, 0, 0)),
         axis.title.y = element_text(vjust = 1),
-        axis.ticks   = element_line(color = "#e3e3e3", size = .2),
+        axis.ticks   = element_line(color = "#e3e3e3", linewidth = .2),
         plot.margin = unit(c(0, 1, .5, 0), "cm")) + 
   coord_flip()
-ggsave("figs/numeracy.png")
-
-
+ggsave("figs/prolific_numeracy.png")
 
 # Uncertainty qs. --- proportion choosing cash
 
 fin_dat <- fin_dat %>% 
   mutate(sure_choice = case_when(
-    cond == "clarifying" ~ uncertain_clarified == "$25 in cash",
-    cond == "original_sure" ~ original_sure == "$25 in cash",
-    cond == "original_uncertain" ~ original_uncertain == "$25 in cash",
-    cond == "direct" ~ direct_comp == "A $50 Amazon gift card"
+    uncertainty_exp == "ariely_choice" ~ ariely_choice == "$25 in cash",
+    uncertainty_exp == "gigerenzer_choice" ~ gigerenzer_choice == "$25 in cash",
+    uncertainty_exp == "certain_choice" ~ certain_choice == "$25 in cash",
+    uncertainty_exp == "uncertain_choice" ~ uncertain_choice == "$25 in cash"
   ))
-
-# Coerce followup to numeric + take out the $1000 response 
-
-fin_dat$followup_n <- as.numeric(fin_dat$followup)
-fin_dat$followup_n[fin_dat$followup_n == 1000] <- NA
 
 ### Analysis
 fin_dat %>%
-  group_by(cond) %>%
+  group_by(uncertainty_exp) %>%
   summarise(mean(sure_choice))
-
-fin_dat %>%
-  group_by(cond) %>%
-  summarise(mean_est = mean(followup_n, na.rm = T), med_est = median(followup_n, na.rm = T))
-
-fin_dat %>%
-  group_by(cond, sure_choice) %>%
-  summarise(mean_est = mean(followup_n, na.rm = T), med_est = median(followup_n, na.rm = T))
 
 #### Let's look at numeracy
 fin_dat %>%
-  group_by(cond, numeracy > .7) %>%
+  group_by(uncertainty_exp, numeracy > .7) %>%
   summarise(mean(sure_choice))
 
-fin_dat %>%
-  filter(numeracy > .7) %>%
-  group_by(cond, sure_choice) %>%
-  summarise(mean_est = mean(followup_n, na.rm = T), med_est = median(followup_n, na.rm = T))
+### Bonus
+fin_dat <- fin_dat %>% 
+  mutate(sure_choice_bonus = case_when(
+    bonus == "certain" ~ bonus_certain == "$0.25 in cash",
+    bonus == "pre" ~ bonus_with_pre_ev == "$0.25 in cash",
+    bonus == "uncertain" ~ bonus_uncertain == "$0.25 in cash",
+  ))
+
+### Analysis
 
 fin_dat %>%
-  group_by(cond, numeracy > .7) %>%
-  summarise(mean_est = mean(followup_n, na.rm = T), med_est = median(followup_n, na.rm = T))
-
-### By follow-up estimate
-fin_dat %>%
-  group_by(cond, followup >= 50) %>%
-  summarise(mean(sure_choice))
+  group_by(bonus, numeracy > .7) %>%
+  summarise(mean(sure_choice_bonus, na.rm = T))
 
